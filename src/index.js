9a6f9dceb6386d3dac5e274e20c565ec76317888
Diego patiño <diegoduvan321@gmail.com>
2023-02-03 10:37:54 -0500

    proyecto e avace

--- src/index.js
+++ src/index.js
@@ -6,6 +6,16 @@
 const baseUrl = "https://platzi-avo.vercel.app";
 
 const appNode = document.querySelector("#app");
+
+  
+
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-EN", {
+    style: "currency",
+    currency: "USD",
+  }).format(price)
+  return newPrice ;
+};
 // web
 //conectarnos al servidor
 window
@@ -18,23 +28,28 @@ window
     const todosLosItems = [];
     responseJson.data.forEach((item) => {
       //crear una imagen
+      
       const imagen = document.createElement("img");
       imagen.className = "imagen";
       imagen.src = `${baseUrl}${item.image}
       `;
-     
-     
+
+      
+
+      
+      
       //crear un title
       const title = document.createElement("h2");
 
       title.textContent = item.name;
       title.className = "title "; //crear un precio
       const price = document.createElement("div");
-      price.className = "price "
-      price.textContent = item.price;
-       
+      price.className = "price ";
+      price.textContent = formatPrice(item.price);
+
       const container = document.createElement("div");
-      container.className = 'container'
+      container.className = "container-items";
+     
       container.append(imagen, title, price);
       todosLosItems.push(container);
     });
